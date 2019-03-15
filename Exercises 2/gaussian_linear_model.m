7794da0ee3f496085542d68c232e5241ab51eb01
afbudak <46981389+afbudak@users.noreply.github.com>
2019-03-15 17:09:09 -0500

    Add files via upload

--- Exercises 2/gaussian_linear_model.m
+++ Exercises 2/gaussian_linear_model.m
@@ -6,10 +6,10 @@ X = data(:,6);                          %defence spending
 X = [ones(length(X),1), X];
 n = length(y);                          %number of sample
 p = size(X,2) - 1;                      %number of variable
-Lambda = diag(n);
+Lambda = eye(n);
 d = 0.01; eta = 0.01;
 m = zeros(p+1, 1); 
-K = 0.005*diag(p);
+K = 0.005*eye(p);
 prod1 = K + transpose(X)*Lambda*X;
 prod2 = K*m + transpose(X)*Lambda*y ;
 mu_star = prod1\prod2;                  %multiplying with inv of prod1
